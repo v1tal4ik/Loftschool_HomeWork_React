bbb110c364ccf5e4dbf2fd38ca077a878692cc22
v1tal4ik <vetalgta@gmail.com>
2018-12-28 15:41:49 +0200

    v.1.2

---
+++ src/components/Show/Show.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+export default class Show extends Component{
+
+    render(){
+        return(
+            <button onClick={this.take}>SEarch!!!</button>
+        )
+    }
+    
+    take(){
+      
+    }
+}
