768928f696d164b8ae9239987e0074b793176557
v1tal4ik <vetalgta@gmail.com>
2019-01-02 16:52:43 +0200

    v.5.0

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,15 @@
 import React, { PureComponent } from 'react';
-import Layout from '../Layout';
-import Footer from '../Footer';
-import Header from '../Header';
-import LoginForm from '../LoginForm';
-import Congratulations from '../Congratulations';
-import { AuthProvider, AuthConsumer } from '../../contexts/Auth';
+import './App.css';
+import Todo from '../Todo';
 
 class App extends PureComponent {
   render() {
     return (
-      <AuthProvider>
-        <Layout header={Header} footer={Footer}>
-          <AuthConsumer>
-            {({ isAuthorized, authorize, authorizeError }) =>
-              isAuthorized ? (
-                <Congratulations />
-              ) : (
-                <LoginForm
-                  authorize={authorize}
-                  authorizeError={authorizeError}
-                />
-              )
-            }
-          </AuthConsumer>
-        </Layout>
-      </AuthProvider>
+      <main className="main">
+        <div className="main__cell">
+          <Todo />
+        </div>
+      </main>
     );
   }
 }
