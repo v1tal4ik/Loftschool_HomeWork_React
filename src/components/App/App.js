c3a9af416f8a2de971821af6e5b74f38928d2a0b
v1tal4ik <vetalgta@gmail.com>
2019-01-02 15:21:23 +0200

    v.4.0

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,74 +1,30 @@
 import React, { PureComponent } from 'react';
-import './App.css';
-
-import Show from '../Show';
-
-import bigBangImage from './assets/bigBang.jpg';
-import houseImage from './assets/house.jpg';
-import santaBarbaraImage from './assets/santaBarbara.jpg';
-
-const radioGroup = [
-  {
-    name: 'House M.D.',
-    apiKey: 'house',
-    image: houseImage
-  },
-  {
-    name: 'Santa Barbara',
-    apiKey: 'santaBarbara',
-    image: santaBarbaraImage
-  },
-  {
-    name: 'The Big Bang Theory',
-    apiKey: 'bigBang',
-    image: bigBangImage
-  }
-];
+import Layout from '../Layout';
+import Footer from '../Footer';
+import Header from '../Header';
+import LoginForm from '../LoginForm';
+import Congratulations from '../Congratulations';
+import { AuthProvider, AuthConsumer } from '../../contexts/Auth';
 
 class App extends PureComponent {
-  state = {
-    selectedShow: ''
-  };
-
-  selectShow = event => {
-    this.setState({ selectedShow: event.target.value });
-  };
-
   render() {
-    const { selectedShow } = this.state;
-
     return (
-      <div className="app">
-        <div className="radio-group">
-          {radioGroup.map(radioInfo => (
-            <div
-              className={`radio ${
-                selectedShow === radioInfo.apiKey ? 'radio--selected' : ''
-              }`}
-              key={radioInfo.name}
-            >
-              <input
-                id={radioInfo.apiKey}
-                className={`radio-input t-radio-input-${radioInfo.apiKey}`}
-                type="radio"
-                name="show"
-                value={radioInfo.apiKey}
-                checked={selectedShow === radioInfo.apiKey}
-                onChange={this.selectShow}
-              />
-              <label htmlFor={radioInfo.apiKey} className="radio-label">
-                <img
-                  className="radio-image"
-                  src={radioInfo.image}
-                  alt={radioInfo.name}
+      <AuthProvider>
+        <Layout header={Header} footer={Footer}>
+          <AuthConsumer>
+            {({ isAuthorized, authorize, authorizeError }) =>
+              isAuthorized ? (
+                <Congratulations />
+              ) : (
+                <LoginForm
+                  authorize={authorize}
+                  authorizeError={authorizeError}
                 />
-                <span className="radio-label">{radioInfo.name}</span>
-              </label>
-            </div>
-          ))}
-        </div>
-        <Show key={selectedShow} showId={selectedShow} />
-      </div>
+              )
+            }
+          </AuthConsumer>
+        </Layout>
+      </AuthProvider>
     );
   }
 }
